29c4247e18ff09aeb259dd567b752c20f4f475a8
ruslanvs <ruslanvs@yahoo.com>
2018-02-08 18:26:54 -0800

    Assignment complete

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import express and path modules.
+var express = require( "express");
+var path = require( "path");
+// Create the express app.
+var app = express();
+// Define the static folder.
+app.use(express.static(path.join(__dirname, "./static")));
+// Setup ejs templating and define the views folder.
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// Root route to render the index.ejs view.
+app.get('/', function(req, res) {
+ res.render("index");
+})
+// Start Node server listening on port 8000.
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+})
